9067162ff23e781e9d01a088632df56bf57cc02e
Allan Victor <allanvictorsilvaandrade@gmail.com>
2026-04-05 11:28:34 -0300

    First Commit

---
+++ backend/rpg-manager/src/test/java/com/allan/rpg_manager/RpgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allan.rpg_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
